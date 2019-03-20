2472af9f5b297cab0122797fb8c164127b6f8c9e
Jakob Buis <jakob.buis@in10.nl>
2019-03-20 13:48:50 +0100

    remove NPM analysis

--- analyse_projects.rb
+++ analyse_projects.rb
@@ -15,26 +15,13 @@ projects.each do |giturl|
         # Clone the project and install dependencies
         `(cd #{directory}; git clone #{giturl} .)`
         `(cd #{directory}; composer install)`
-        `(cd #{directory}; npm install)`
 
         # Run checks
         composer_outdated = `(cd #{directory}; composer outdated -D --no-ansi)`.lines.count
 
-        cmd = `(cd #{directory}; npm outdated --json --depth=0)`
-        npm_outdated = JSON.parse(cmd).count
-
-        cmd = `(cd #{directory}; npm audit --json)`
-        npm_audits = JSON.parse(cmd)
-        counts = npm_audits['metadata']['vulnerabilities']
-        total_issues = counts.values.reduce :+
-        high_issues = counts['critical'] + counts['high']
-
         # store results
         results[giturl] = {
             composer: composer_outdated,
-            npm: npm_outdated,
-            total_vulnerabilities: total_issues,
-            high_vulnerabilities: high_issues,
         }
     end
 end
