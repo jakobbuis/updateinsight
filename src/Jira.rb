c2b46d3abbad8ccc5a3b735e62a2224a98a39aac
Jakob Buis <jakob.buis@in10.nl>
2019-05-21 15:41:59 +0200

    add exception handling and logging when the call to JIRA fails

--- src/Jira.rb
+++ src/Jira.rb
@@ -16,15 +16,21 @@ class Jira
     def make_ticket! project
         @logger.info "Creating a JIRA issue for #{project['jira']}"
         issue = @client.Issue.build
-        issue.save({
-            'fields' => {
-                'summary' => "Composer update nodig voor #{project['github']}",
-                'project' => { 'key' => project['jira']},
-                'issuetype' => { 'id' => '10102' }, # type BUG
-                'customfield_11400' => { 'value' => "Ja" }, # SLA ja/nee
-                'labels' => ['backend', 'updateinsight'],
-            },
-        })
+        begin
+            issue.save!({
+                'fields' => {
+                    'summary' => "Composer update nodig voor #{project['github']}",
+                    'project' => { 'key' => project['jira']},
+                    'issuetype' => { 'id' => '10102' }, # type BUG
+                    'customfield_11400' => { 'value' => "Ja" }, # SLA ja/nee
+                    'labels' => ['backend', 'updateinsight'],
+                },
+            })
+        rescue JIRA::HTTPError => exception
+            data = JSON.parse(exception.response.body)
+            @logger.error "Cannot create issue in JIRA: #{data['code']} - #{data['message']}"
+            return
+        end
         @logger.info "Created issue #{issue.key}"
     end
 end
