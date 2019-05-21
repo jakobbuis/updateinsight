ccedf7f9222d2ed1207eea2be6e10efd3d693c40
Jakob Buis <jakob.buis@in10.nl>
2019-05-21 15:05:19 +0200

    use JSON output of composer outdated

--- src/Analysers/Composer.rb
+++ src/Analysers/Composer.rb
@@ -12,22 +12,15 @@ module Analysers
                 return false
             end
 
+            # Run composer analysis
             @logger.info "Analysing composer dependencies"
-            # Parse composer outdated output
-            packages = `composer outdated -D --no-ansi`.split("\n").map do |line|
-                line.downcase!
-                line.scan /^([a-z0-9]+\/[a-z0-9]+) +([0-9a-z\.]+) +([!~=]) +([0-9a-z\.]+)/
-            end
-            packages.flatten! 1
-
-            # Subdivide into sets based on upgrade path
-            minor = packages.select { |p| p[2] === '!' }
-            major = packages.select { |p| p[2] === '~' }
+            command = `composer outdated -oDm --strict -f=json`
+            packages = JSON.parse(command)
 
             # Create a JIRA-ticket if minor updates are required
-            updates = minor.count > 0 || major.count > 0
-            @logger.info(updates ? "Composer updates needed" : "Composer updates not needed")
-            updates
+            update_needed = packages.installed.count > 0
+            @logger.info(update_needed ? "Composer updates needed" : "Composer updates not needed")
+            update_needed
         end
     end
 end
