e86a2316115b7d41c787a7891911995eab8689c7
Jakob Buis <jakob.buis@in10.nl>
2019-06-01 14:13:34 +0200

    if a ticket already exists for a project, increase its priority rather than creating a duplicate ticket

--- src/Jira.rb
+++ src/Jira.rb
@@ -14,6 +14,36 @@ class Jira
     end
 
     def make_ticket! project
+        # If a ticket has been created in a previous month, and it is not done yet.
+        # We bump its priority instead, and not create a duplicate ticket.
+        @logger.info "Checking for an in-progress ticket for #{project['jira']}"
+        query = "status != Done AND labels = updateinsight AND Summary ~ \"#{project['github']}\""
+        results = @client.Issue.jql(query)
+        if results.count > 0
+            increase_priority results[0]
+        else
+            create_new_ticket project
+        end
+    end
+
+    private
+
+    def increase_priority issue
+        current_priority = issue.priority.id.to_i
+        new_priority = [1, current_priority - 1].max.to_s # priority IDs go from 5 - Lowest to 1 - Highest
+        @logger.info "Increasing the priority of #{issue.key} from #{current_priority} to #{new_priority}"
+        begin
+            issue.save!({
+                'fields' => { 'priority' => { 'id' => new_priority } }
+            })
+        rescue JIRA::HTTPError => exception
+            @logger.error "Cannot increase priority: #{exception.response.body}"
+            return
+        end
+        @logger.info "Increased priority of #{issue.key} to #{new_priority}"
+    end
+
+    def create_new_ticket project
         @logger.info "Creating a JIRA issue for #{project['jira']}"
         issue = @client.Issue.build
         begin
